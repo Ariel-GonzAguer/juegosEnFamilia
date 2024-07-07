5ad7d7118c870c8d1b78c13b947acc3f6b621bb3
ArieGonzAguer <ariegonzaguer@gmail.com>
2024-07-07 16:38:42 -0600

    starting project

---
+++ juegos-basicos/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./App.module.css";
+
+export default function App() {
+  return <section className={styles.home}>
+    <h1>Hola mundo</h1>
+  </section>
+}
---
+++ juegos-basicos/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import SLICES from './SLICES'
+
+const store = configureStore({
+  reducer: {
+    // SLICE: REDUCER
+  }
+})
+
+export default store
